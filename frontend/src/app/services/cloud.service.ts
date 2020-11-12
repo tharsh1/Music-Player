f3f138adfb4fa76b8b5f390ad3e7cce827f1682c
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-11-12 18:24:07 +0530

    styles fix

--- frontend/src/app/services/cloud.service.ts
+++ frontend/src/app/services/cloud.service.ts
@@ -14,6 +14,36 @@ export class CloudService {
       artist: ' Ed Sheeran',
     },
     {
+      url:
+        'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
+      name: 'Perfect',
+      artist: ' Ed Sheeran',
+    },
+    {
+      url:
+        'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
+      name: 'Perfect',
+      artist: ' Ed Sheeran',
+    },
+    {
+      url:
+        'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
+      name: 'Perfect',
+      artist: ' Ed Sheeran',
+    },
+    {
+      url:
+        'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
+      name: 'Perfect',
+      artist: ' Ed Sheeran',
+    },
+    {
+      url:
+        'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
+      name: 'Perfect',
+      artist: ' Ed Sheeran',
+    },
+    {
       // tslint:disable-next-line: max-line-length
       url:
         'https://ia801609.us.archive.org/16/items/nusratcollection_20170414_0953/Man%20Atkiya%20Beparwah%20De%20Naal%20Nusrat%20Fateh%20Ali%20Khan.mp3',
